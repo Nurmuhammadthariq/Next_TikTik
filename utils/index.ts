4a02fd6a18d7dc7dc4a6b15ce4560a2bed2e1de2
thoriq.noer <thoriq.noer@gmail.com>
2023-01-29 11:35:07 +0700

    add: reafactor base url

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 import jwt_decode from 'jwt-decode'
 
-export const createOrGetUser = async (response: any) => {
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+
+export const createOrGetUser = async (response: any, addUser: any) => {
 	const decode: { name: string, picture: string, sub: string } = jwt_decode(response.credential)
 
 	const { name, picture, sub } = decode
@@ -12,5 +14,7 @@ export const createOrGetUser = async (response: any) => {
 		image: picture
 	}
 
-	await axios.post(`http://localhost:3000/api/auth`, user)
+	addUser(user)
+
+	await axios.post(`${BASE_URL}/api/auth`, user)
 }
